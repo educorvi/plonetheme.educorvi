16b440b688b193f68d92205c2e4a30a9189a5f7c
Lars Walther <lars.walther@educorvi.de>
2024-01-07 17:35:47 +0100

    initial commit

--- src/plonetheme/educorvi/viewlets/banner_viewlet.py
+++ src/plonetheme/educorvi/viewlets/banner_viewlet.py
@@ -8,6 +8,9 @@ class BannerViewlet(ViewletBase):
 
     def update(self):
         self.imageurl = api.portal.get().absolute_url() + '/++plone++plonetheme.educorvi'
+        self.consulting = 'consulting' in self.context.id
+        self.development = 'development' in self.context.id
+        self.education = 'education' in self.context.id
 
 
     def index(self):
--- src/plonetheme/educorvi/views/home_view.py
+++ src/plonetheme/educorvi/views/home_view.py
@@ -9,8 +9,7 @@ class HomeView(BrowserView):
 
     def __call__(self):
         self.res_url = api.portal.get().absolute_url()+'/++plone++plonetheme.educorvi'
-        return self.index()
-        titleparts = self.context.title.split('@')[0]
+        titleparts = self.context.title.split('@')
         self.title_one = titleparts[0]
         self.title_two = ''
         if len(titleparts) > 1:
@@ -18,6 +17,15 @@ class HomeView(BrowserView):
         self.cat_one = self.context.service_categories[0]
         self.cat_two = self.context.service_categories[1]
         self.cat_three = self.context.service_categories[2]
+        self.services = self.get_services()
+        return self.index()
 
-    def get_header_content(self):
-        title = self.context.title
+    def get_services(self):
+        services = []
+        for service in self.context.getFolderContents():
+            obj = service.getObject()
+            entry = {}
+            entry['object'] = obj
+            entry['image_url'] = obj.absolute_url() + '/@@download/image/' + obj.image.filename
+            services.append(entry)
+        return services
