df5e5190577618c10abdd3a9e8b37668de3924bb
Lars Walther <lars.walther@educorvi.de>
2024-01-14 12:21:27 +0100

    Änderungen

--- src/plonetheme/educorvi/viewlets/references_viewlet.py
+++ src/plonetheme/educorvi/viewlets/references_viewlet.py
@@ -6,10 +6,27 @@ from plone.app.layout.viewlets import ViewletBase
 class ReferencesViewlet(ViewletBase):
 
     def update(self):
-        self.message = self.get_message()
+        self.references = self.get_references()
 
-    def get_message(self):
-        return u'My message'
-
+    def get_references(self):
+        refs = []
+        for entry in self.context.contentcards:
+            ref = {}
+            if entry.to_object:
+                obj = entry.to_object
+                ref['title'] = obj.title
+                ref['description'] = obj.description
+                ref['url'] = obj.absolute_url()
+                if hasattr(obj, 'remoteUrl'):
+                    ref['url'] = obj.remoteUrl
+                ref['img'] = ''
+                if hasattr(obj, 'image'):
+                    ref['img'] = obj.absolute_url() + '/@@images/image/mini'
+                refs.append(ref)
+        return refs
+                
     def index(self):
-        return super(ReferencesViewlet, self).render()
+        if hasattr(self.context, 'contentcards'):
+            if self.context.contentcards:
+                return super(ReferencesViewlet, self).render()
+        return ""
