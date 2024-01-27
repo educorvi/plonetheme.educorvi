12a80712d834c135ede8e62199b45d080957f129
Lars Walther <larswalther@Laptop-von-Lars.fritz.box>
2024-01-28 00:10:26 +0100

    favicon-Handling, Banner-Viewlet überarbeitet

--- src/plonetheme/educorvi/viewlets/banner_viewlet.py
+++ src/plonetheme/educorvi/viewlets/banner_viewlet.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from plone import api
 from plone.app.layout.viewlets import ViewletBase
 
@@ -7,11 +6,14 @@ from plone.app.layout.viewlets import ViewletBase
 class BannerViewlet(ViewletBase):
 
     def update(self):
-        self.imageurl = api.portal.get().absolute_url() + '/++plone++plonetheme.educorvi'
+        portal_url = api.portal.get().absolute_url()
+        self.imageurl = portal_url + '/++plone++plonetheme.educorvi'
+        self.consulting_url = f'{portal_url}/consulting'
+        self.development_url = f'{portal_url}/development'
+        self.education_url = f'{portal_url}/education'
         self.consulting = 'consulting' in self.context.id
         self.development = 'development' in self.context.id
         self.education = 'education' in self.context.id
-
 
     def index(self):
         return super(BannerViewlet, self).render()
